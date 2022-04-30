87ff051de744266d12379169a2d4568fd8f18e92
Kota <maedakoutaa@gmail.com>
2022-05-01 01:25:57 +0900

    Lottieの土台の実装は完了。細かな修正はしたい。

---
+++ SwipeToDoApp/Controller/Lottie01ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  Lottie01ViewController.swift
+//  SwipeToDoApp
+//
+//  Created by 前田航汰 on 2022/04/30.
+//
+
+import UIKit
+import Lottie
+
+class Lottie01ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showAnimation()
+    }
+
+    func showAnimation() {
+        let animationView = AnimationView(name: "LottieWriteBoy")
+        animationView.frame = CGRect(x: 0, y: 0, width: view.frame.width-80, height: view.bounds.height-500)
+        animationView.center = self.view.center
+        animationView.loopMode = .loop
+        animationView.contentMode = .scaleAspectFit
+        animationView.animationSpeed = 1
+
+        view.addSubview(animationView)
+        animationView.play()
+    }
+    // アラート表示させるのもあり
+    @IBAction func didTapSkipButton(_ sender: Any) {
+        let firstLunchKey = "firstLunchKey"
+        UserDefaults.standard.set(true, forKey: firstLunchKey)
+
+        let TabBarController = storyboard?.instantiateViewController(withIdentifier: "MainTabBarController") as! UITabBarController
+        self.present(TabBarController, animated: true, completion: nil)
+    }
+
+}
