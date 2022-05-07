28cc937c0e361d3bf8e62d111b22a5cce55a517b
Kota <maedakoutaa@gmail.com>
2022-05-08 08:24:38 +0900

    操作方法画面に戻るボタン進むボタンを実装

--- SwipeToDoApp/Controller/SettingMethodOfOperationViewController.swift
+++ SwipeToDoApp/Controller/SettingMethodOfOperationViewController.swift
@@ -12,6 +12,9 @@ import PKHUD
 class SettingMethodOfOperationViewController: UIViewController, WKUIDelegate, WKNavigationDelegate  {
 
     @IBOutlet private weak var webView: WKWebView!
+    @IBOutlet private weak var backButton: UIBarButtonItem!
+    @IBOutlet private weak var forwardButton: UIBarButtonItem!
+
     private let operationUrl = "https://local-tumbleweed-7ea.notion.site/SwipeToDo-ea54cf669ca246a997f091803dbbb04e"
 
     override func viewDidLoad() {
@@ -25,15 +28,45 @@ class SettingMethodOfOperationViewController: UIViewController, WKUIDelegate, WK
         } else {
             print("操作方法のURLが取得できませんでした。")
         }
+        judgeToolBarButton()
+    }
+
+    @IBAction func didTapBackButton(_ sender: Any) {
+        webView.goBack()
+    }
+
+    @IBAction func didTapForwardButton(_ sender: Any) {
+
+        webView.goForward()
+    }
+
+    func webView(_ webView: WKWebView, contextMenuWillPresentForElement elementInfo: WKContextMenuElementInfo) {
+        print("???")
     }
 
     func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
         print("操作方法の読み込み開始")
+        judgeToolBarButton()
     }
 
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         print("操作方法の読み込み完了")
         HUD.hide(animated: true)
+        judgeToolBarButton()
+    }
+
+    private func judgeToolBarButton() {
+        if webView.canGoBack {
+            backButton.isEnabled = true
+        } else {
+            backButton.isEnabled = false
+        }
+
+        if webView.canGoForward {
+            forwardButton.isEnabled = true
+        } else {
+            forwardButton.isEnabled = false
+        }
     }
 
     @IBAction private func didTapExitButton(_ sender: UIBarButtonItem) {
